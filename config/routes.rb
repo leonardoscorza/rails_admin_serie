56266d6b369049013a8d67245db02207e73fc33f
Leonardo Scorza <leonardoscorzas88@gmail.com>
2017-02-04 13:40:18 -0200

    first part

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   mount RailsAdmin::Engine => '/', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
